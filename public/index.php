1a212bdaf9e0c9eae9b471a5e9e601415a0bd9ce
Elias Fellinger <efellinger.mmt-b2021@fh-salzburg.ac.at>
2022-12-30 16:05:01 +0100

    fix: reinstalled wordpress

--- public/index.php
+++ public/index.php
@@ -11,11 +11,6 @@
  *
  * @var bool
  */
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
