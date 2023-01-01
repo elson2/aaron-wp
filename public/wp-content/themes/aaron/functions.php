657d5ccd3b386f6178ee2eed21fddc1f9a843b5c
Elias Fellinger <efellinger.mmt-b2021@fh-salzburg.ac.at>
2023-01-01 21:20:08 +0100

    test: changed svg support function

--- public/wp-content/themes/aaron/functions.php
+++ public/wp-content/themes/aaron/functions.php
@@ -6,14 +6,10 @@ function wpdocs_theme_name_scripts()
 }
 add_action('wp_enqueue_scripts', 'wpdocs_theme_name_scripts');
 
-function cc_mime_types($mimes)
-{
+function cc_mime_types($mimes) {
 	$mimes['svg'] = 'image/svg+xml';
 	return $mimes;
 }
-add_filter(
-	'upload_mimes',
-	'cc_mime_types'
-);
 
+add_filter('upload_mimes', 'cc_mime_types');
 add_theme_support('post-thumbnails');
