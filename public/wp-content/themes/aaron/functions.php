7f75c4d9f3ebbbcab2279b655b3a9246049dbdcf
David <dgrois.mmt-b2021@fh-salzburg.ac.at>
2023-01-01 21:24:23 +0100

    functions change

--- public/wp-content/themes/aaron/functions.php
+++ public/wp-content/themes/aaron/functions.php
@@ -6,10 +6,21 @@ function wpdocs_theme_name_scripts()
 }
 add_action('wp_enqueue_scripts', 'wpdocs_theme_name_scripts');
 
-function cc_mime_types($mimes) {
-	$mimes['svg'] = 'image/svg+xml';
-	return $mimes;
-}
+// function cc_mime_types($mimes) {
+// 	$mimes['svg'] = 'image/svg+xml';
+// 	return $mimes;
+// }
 
-add_filter('upload_mimes', 'cc_mime_types');
+// add_filter('upload_mimes', 'cc_mime_types');
 add_theme_support('post-thumbnails');
+
+function add_file_types_to_uploads($file_types)
+{
+
+	$new_filetypes = array();
+	$new_filetypes['svg'] = 'image/svg+xml';
+	$file_types = array_merge($file_types, $new_filetypes);
+
+	return $file_types;
+}
+add_action('upload_mimes', 'add_file_types_to_uploads');
