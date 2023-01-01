d5f20d2ad02fc07e66203dc2525b2df689fec9fe
David <dgrois.mmt-b2021@fh-salzburg.ac.at>
2023-01-01 19:13:20 +0100

    svg support

--- public/wp-content/themes/aaron/functions.php
+++ public/wp-content/themes/aaron/functions.php
@@ -11,5 +11,9 @@ function cc_mime_types($mimes)
 	$mimes['svg'] = 'image/svg+xml';
 	return $mimes;
 }
-add_filter('upload_mimes', 'cc_mimes_types');
+add_filter(
+	'upload_mimes',
+	'cc_mime_types'
+);
+
 add_theme_support('post-thumbnails');
